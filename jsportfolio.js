4912ee64f5cfcd596b5df49aec56f327e40b9d39
yigitsktas <yigitsktas@gmail.com>
2022-05-23 21:52:13 +0300

    fixed portfolia page button by using another js file, removed shape change of button cause of responsiveness

---
+++ jsportfolio.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const portfolioButtonHome = document.querySelector(".btn-home");
+
+portfolioButtonHome.addEventListener("click", function () {
+	window.open("home.html", "_top");
+});
